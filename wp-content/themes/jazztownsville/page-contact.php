5946c487ecb414000b487d37acf76753042a9aa7
Zaeed <zaeed.kibria@my.jcu.edu.au>
2021-05-18 15:09:47 +1000

    Contact (php) file updated for the theme

---
+++ wp-content/themes/jazztownsville/page-contact.php
@@ -0,0 +1,30 @@
+<?php 
+get_header();
+?>
+
+
+<div class="container-fluid bg-darked">
+    <div class="row">
+        <div style="color: white;" class="py-5 col-md-12 col-lg-12 col-xl-12 col-sm-12 col-xs-12">
+            <center><h1 class="col-sm-4 col-xs-4 col-md-4 col-lg-3" style="text-align:left; font-family: 'Nunito', sans-serif; font-weight:600; font-size: 70px;">Looking <br /> to contact <br /> us?</h1>
+        
+            <h4 class="pt-5">WHO TO CALL?</h4>
+            <h4><strong style="color: #f1d058;">CORALIE COSTIGAN</strong> 0431618618</h4>
+            
+            
+            <h4 class="pt-5">WHERE TO GO?</h4>
+            <h4><strong style="color: #f1d058;">FISH INN ROCKPOOL</strong></h4>
+            <h4>54 HOWITT ST, TOWNSVILLE QLD</h4>
+            <h4>4810</h4>
+            
+            <h4 class="pt-5">LOOKING TO WRITE</h4>
+            <h4 style="color: #f1d058;">TOWNVILLEJAZZ@ICLOUD.COM</h4>
+        </center>
+        </div>
+    </div>
+</div>
+    
+
+<?php
+    get_footer();
+?>
