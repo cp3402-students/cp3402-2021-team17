25976e27ad84e172ba58fac4b1680222149ff4a9
Zaeed <zaeed.kibria@my.jcu.edu.au>
2021-05-18 15:00:53 +1000

    Template for displaying the footer

---
+++ wp-content/themes/jazztownsville/footer.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package jazztownsville
+ */
+
+?>
+
+	<footer style="position: relative; bottom: 0px;">
+	    <div class="container-fluid">
+	        <div class="row">
+	            <div class="p-3 col-md-12 col-lg-12 col-xs-12 col-sm-12">
+	                <center>
+	                    <h4 style="font-size: 15px; font-family: 'Spartan', sans-serif; font-weight: 500;">COPYRIGHT | TOWNVILLE JAZZ CLUB</h4>
+	                </center>
+	            </div>
+	        </div>
+	    </div>
+	</footer><!-- #colophon -->
+<?php wp_footer(); ?>
+</body>
+</html>
