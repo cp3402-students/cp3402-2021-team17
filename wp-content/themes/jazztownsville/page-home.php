6edc3da7df2a96a9f29e385f3af702894ad69fda
Zaeed <zaeed.kibria@my.jcu.edu.au>
2021-05-18 15:08:57 +1000

    Homepage (php) file updated for the theme

---
+++ wp-content/themes/jazztownsville/page-home.php
@@ -0,0 +1,57 @@
+<?php
+    get_header();
+?>
+
+<div class="container-fluid bg-darked" style="color: white;">
+    <div class="container">
+        <div class="row">
+            <div class="py-5 col-md-12 col-lg-12 col-xs-12 col-sm-12">
+            <center>
+                    <h1 style="font-family: 'GlacialIndifferenceRegular'; margin:0px !important; font-weight: 100;">Townville  Jazz  club</h1>
+                    <h2 style="font-family: 'GlacialIndifferenceRegular'; font-weight: 100;">presents</h2>
+            </center>
+            </div>
+            
+            <div class="my-4 d-flex justify-content-end col-md-6 col-lg-6 col-sm-6 col-xs-6">
+                <h1 style=" margin:0px !important; color: #f1d058; font-family: 'Spartan', sans-serif !important; font-weight:900; font-size: 100px;">Jazz
+                </br>
+                Night
+                </h1>
+            </div>
+            
+            
+            <div class="my-4 col-md-6 col-lg-6 col-sm-6 col-xs-6 pt-4">
+                <h3 style="font-family: 'Spartan', sans-serif; font-weight:500; ">21ST JUNE</h3>
+                <h3 style="font-family: 'Spartan', sans-serif; font-weight:100; ">54 HOWITT ST, </h3> 
+                <h3 style="font-family: 'Spartan', sans-serif; font-weight:100; ">TOWNSVILLE 4810</h3>
+            </div>
+            
+            
+            <div class="my-5 col-md-12 col-lg-12 col-sm-12 col-xs-12">
+                <center><img src="<?php echo get_template_directory_uri() . '/1.png' ?>" width="300" height="200" /></center>
+            </div>
+            
+            <div class="col-md-6 col-lg-6 col-sm-6 col-xs-6 py-5">
+                <center>
+                <h3 class="pb-3" style="font-family: 'Spartan', sans-serif; font-weight:700;">MEMBERSHIP PRICE</h3>
+                <h4 style="font-family: 'Marta', sans-serif;">Members 10$</h4>
+                <h4 style="font-family: 'Marta', sans-serif;">Non Members 15$</h4></center>
+            </div>
+            
+            
+            <div class="col-md-6 col-lg-6 col-sm-6 col-xs-6 py-5">
+                <center>
+                <h3 class="pb-3" style="font-family: 'Spartan', sans-serif; font-weight:700; ">SERVICES OFFERED</h3>
+                   <h4 style="font-family: 'Marta', sans-serif;">Seafood Menu</h4>
+                    <h4 style="font-family: 'Marta', sans-serif;">Bar Facility</h4>
+                </center>
+            </div>
+            
+            
+        </div>
+    </div>
+</div>
+
+<?php
+    get_footer();
+?>
